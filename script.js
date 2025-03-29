d7d2a244ac73e94f3ffc7e723d417f31e57f1afd
Divyapanerkar31 <divyapanerkar31@gmail.com>
2025-03-29 19:51:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeTextColor(color){
+    let h2 = document.getElementById('head')
+    h2.style.color=color
+}
+function changebackgroundColor(color){
+    let h2 = document.getElementById('head')
+    h2.style.backgroundColor=color
+}
+function changeFont(font){
+    let h2 = document.getElementById('head')
+    h2.style.fontFamily=font
+}
